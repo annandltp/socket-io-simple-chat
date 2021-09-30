9e0ffc91396bda3b4315bc17bc40a6339702e8d2
Anan <anan.dela211@gmail.com>
2021-09-30 22:41:59 +0700

    u

---
+++ app.js
@@ -0,0 +1,25 @@
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+let countUserOnline = 1
+io.on('connection', socket => {
+    socket.on('join', param => {
+        console.log('user join')
+        countUserOnline++
+        io.emit('countUserOnline', countUserOnline)
+    })
+    socket.on('message', param => {
+        console.log('user message')
+        io.emit('message', param)
+    })
+    socket.on('disconnect', param => {
+        console.log('user disconnect')
+        countUserOnline--
+        io.emit('countUserOnline', countUserOnline)
+    })
+})
+server.listen(3000)
